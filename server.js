d7b9dd9cb9b60793ef6d19dc43ca6323f6703cef
Sydney Mercier <sydney.mercier91@gmail.com>
2022-02-09 19:47:21 -0600

    created main menu in inquirer

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
-const express = require('express');
 const mysql = require('mysql2');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+const inquirer = require('inquirer');
+const consoleTable = require('console.table');
 
 // connect to database
 const db = mysql.createConnection(
@@ -15,6 +9,96 @@ const db = mysql.createConnection(
         user: 'root',
         password: 'pass',
         database: 'employees_db'
-    },
-    console.log('Connected to the employees_db database.')
+    }
 );
+
+// connect to database and start app
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to the employees_db database.');
+    mainMenu();
+});
+
+// displays the main menu of choices available to the user
+function mainMenu() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                pageSize: 8,
+                name: "options",
+                message: "What would you like to do?",
+                choices: [
+                    "View All Departments",
+                    "View All Roles",
+                    "View All Employees",
+                    "Add a Department",
+                    "Add a Role",
+                    "Add an Employee",
+                    "Update an Employee Role",
+                    "Exit App"
+                ]
+            }
+        ])
+        .then((response) => {
+            // move into submenus
+            switch (response.options) {
+                case "View All Departments":
+                    viewAllDepartments();
+                    break;
+                case "View All Roles":
+                    viewAllRoles();
+                    break;
+                case "View All Employees":
+                    viewAllEmployees();
+                    break;
+                case "Add a Department":
+                    addDepartment();
+                    break;
+                case "Add a Role":
+                    addRole();
+                    break;
+                case "Add an Employee":
+                    addEmployee();
+                    break;
+                case "Update an Employee Role":
+                    updateEmployeeRole();
+                    break;
+                case "Exit App":
+                    db.end();
+                    break;
+                default:
+                    console.log("Invalid choice");
+            }
+        });
+}
+
+function viewAllDepartments() {
+
+}
+
+function viewAllRoles() {
+
+}
+
+function viewAllEmployees() {
+
+}
+
+function addDepartment() {
+
+}
+
+function addRole() {
+
+}
+
+function addEmployee() {
+
+}
+
+function updateEmployeeRole() {
+
+}
